58cd6d23f4436f017121b90956fb3d51d711369d
KlimovCode <klimovcode@mail.ru>
2021-03-15 10:03:26 +0500

    fix route

--- project/config/routes.php
+++ project/config/routes.php
@@ -5,7 +5,7 @@
 		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
 		new Route('/my-page1/', 'page', 'show1'),
 		new Route('/my-page2/', 'page', 'show2'),
-		new Route('/act/:id/', 'page', 'show'),
+		new Route('/page/:id/', 'page', 'show'),
 		new Route('/act/:ar/', 'page', 'act'),
 		new Route('/product/:id/', 'product', 'show'),
 		new Route('/nums/:n1/:n2/:n3', 'num', 'numsum')
